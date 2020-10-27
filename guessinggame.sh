49c9ab989dd25c647791a508046dda73a80a1eae
AnuragShandilyaM <71324801+AnuragShandilyaM@users.noreply.github.com>
2020-10-27 18:04:11 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+
 function guessloop {
-  while [[ $guess -ne $file_number ]]
-  do 
-    if [[ $guess -lt $file_number ]]
-    then
-      echo "Your guess is lower. Please enter a number again:"
-      read guess
-    elif [[ $guess -gt file_number ]]
-    then
-      echo "Your guess is greater. Please enter a number again:"
-      read guess
-    fi
-  done
-  echo "Congratulations! Your answer is correct!"
+
+while [[ $guess -ne $file_number ]]
+do
+	if [[ $guess -lt $file_number ]]
+	then
+	    echo "Your guess is lower. Please enter a number again:"
+	    read guess
+	     
+ 	elif [[ $guess -gt $file_number ]]
+ 	then
+ 	    echo "Your guess is greater. Please enter a number again:"
+ 	    read guess
+ 	fi
+done
+echo "Congratulations! Your answer is correct!"
 }
 
 echo "Plese enter how many files are in the current directory:"
