3f68f4326c293babb182f72c6089186f1a50c365
AnuragShandilyaM <71324801+AnuragShandilyaM@users.noreply.github.com>
2020-10-27 12:07:29 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+function guessloop {
+  while [[ $guess -ne $file_number ]]
+  do 
+    if [[ $guess -lt $file_number ]]
+    then
+      echo "Your guess is lower. Please enter a number again:"
+      read guess
+    elif [[ $guess -gt file_number ]]
+    then
+      echo "Your guess is greater. Please enter a number again:"
+      read guess
+    fi
+  done
+  echo "Congratulations! Your answer is correct!"
+}
+
+echo "Plese enter how many files are in the current directory:"
+read guess
+file_number=$(ls | wc -l)
+guessloop guess file_number
